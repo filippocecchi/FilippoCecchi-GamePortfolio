0477468c9a761644727386c801e05b380ab6dd52
Filippo Cecchi <filippoc.2002@gmail.com>
2024-05-30 20:41:45 +0200

    Changed Text

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,8 @@ export const dialogueData = {
     
     "sofa-tv": `That's my Relax Area. I've been watching for the last 3 years only business and tech related content. The only content I watch beside those is online chess game analysis`,
     bed: `This is where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    degree: `I have a Computer Science & Management Degree from Univeristy of Bologna, I also attended several online courses from top certified schools. Check it out from my Linkedin profile, <a href="https://www.linkedin.com/in/filippo-cecchi-b64716236/" target="_blank">Check it out</a>.`,
-    computer: `This is my desk and on it is my resume. Here is where I always work and study new things. I love learning. Here the link to my resume and to my Github profile, <a href="https://github.com/filippocecchi" target="_blank">Check it out</a>. Or Contact me at filippoc.2002@gmail.com if you have any interesting job opportunities!`,
-    bookshelf: `I love reading a lot. I only enjoy self-improvement, psychology, business and human behaviour books. Sometimes I read famous entrepreneurs biography. Check out the full list of books that I have read, <a href="" target="_blank">Github</a>Click Here</a>!`,
+    degree: `I'm studying for a Computer Science & Management Degree from Univeristy of Bologna, I also attended several online courses from top certified schools. Check it out from my Linkedin profile, <a href="https://www.linkedin.com/in/filippo-cecchi-b64716236/" target="_blank">Check it out</a>.`,
+    computer: `This is my desk and on it is my resume. Here is where I always work and study new things. I love learning. Here the link to my resume and to my Github profile, <a href="https://github.com/filippocecchi" target="_blank">Check it out</a>. Or Contact me at filippoc.2002@gmail.com if you have any interesting opportunities!`,
+    bookshelf: `I love reading a lot. I enjoy self-improvement, psychology, business and human behaviour books. Sometimes I read famous entrepreneurs biography. I'm always looking for new books to read, so if you have any suggestions, please let me know!`,
     exit: `If you want to exit Filippo Cecchi's portfolio, just close the tab.`,
   };
