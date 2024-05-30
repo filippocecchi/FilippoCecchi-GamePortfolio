8c2adcf0bda2335a0cb252c0f1e9e45e43ab048e
Filippo Cecchi <filippoc.2002@gmail.com>
2024-05-30 20:48:01 +0200

    Change Sofa

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const scaleFactor=4;
 export const dialogueData = {
     fridge: `This is my fridge. Full of healthy food that I normally it to keep myself in shape!`,
     
-    "sofa-tv": `That's my Relax Area. I've been watching for the last 3 years only business and tech related content. The only content I watch beside those is online chess game analysis`,
+    "sofa-tv": `Sometimes I like watching business and tech related content. The only content I watch beside those is online chess games and sometimes football`,
     bed: `This is where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
     degree: `I'm studying for a Computer Science & Management Degree from Univeristy of Bologna, I also attended several online courses from top certified schools. Check it out from my Linkedin profile, <a href="https://www.linkedin.com/in/filippo-cecchi-b64716236/" target="_blank">Check it out</a>.`,
     computer: `This is my desk and on it is my resume. Here is where I always work and study new things. I love learning. Here the link to my resume and to my Github profile, <a href="https://github.com/filippocecchi" target="_blank">Check it out</a>. Or Contact me at filippoc.2002@gmail.com if you have any interesting opportunities!`,
